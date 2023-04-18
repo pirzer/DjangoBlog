a3c8e437a92cfbd4ab3b6641b6e2e4ebee35a5c6
pirzer <pirzer1@yahoo.com>
2023-04-18 22:39:28 +0000

    p4 settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['codestarp4.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar.herokuapp.com', 'localhost']
 
 
 # Application definition
