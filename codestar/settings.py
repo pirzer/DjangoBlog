0cda132c4d4f0853b0339cd049fe5ffad7f8cc1f
pirzer <pirzer1@yahoo.com>
2023-04-19 20:51:05 +0000

    adding https://codestarp4.herokuapp.com/ to ALLOWED HOST in settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,7 +34,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['codestar.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestarp4.herokuapp.com/', 'localhost']
 
 
 # Application definition
